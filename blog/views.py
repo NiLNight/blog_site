5b365c8cff48b19deedef970ffbca1471e795280
NiLNight <bkmz-2020@inbox.ru>
2024-06-19 19:40:06 +0300

    обновление бд

--- blog/views.py
+++ blog/views.py
@@ -9,13 +9,10 @@ def post_list(request):
                   {'posts': posts})
 
 
-def post_detail(request, year, month, day, post):
+def post_detail(request, id):
     post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED,
-                             slug=post,
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
+                             id=id,
+                             status=Post.Status.PUBLISHED)
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
