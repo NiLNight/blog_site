6456d9f000596a57559a33eeab93d0e1339e8b9d
NiLNight <bkmz-2020@inbox.ru>
2024-06-19 19:31:13 +0300

    создание удобных url адресов

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,8 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='blog_post')
     body = models.TextField()
@@ -37,3 +38,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail",
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
