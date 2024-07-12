b6fdfb6916175df9646280bd63646c9cffbb3c2d
NiLNight <bkmz-2020@inbox.ru>
2024-07-12 18:50:56 +0300

    добавлена простого поиска, новостных лент и переход на postgreSQL

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,8 @@ from taggit.models import Tag
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, \
     PageNotAnInteger
-from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from django.contrib.postgres.search import SearchVector
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.views.decorators.http import require_POST
 from django.db.models import Count
 
@@ -116,3 +116,22 @@ def post_comment(request, post_id):
                   {'post': post,
                    'form': form,
                    'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query)
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
