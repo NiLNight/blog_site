0424eef0d4f75efe563c2f8abc75357318440590
NiLNight <bkmz-2020@inbox.ru>
2024-07-13 18:05:33 +0300

    не большие доработки

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
@@ -9,6 +10,7 @@ sitemaps = {
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('blog:post_list') ),
     path('blog/', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
